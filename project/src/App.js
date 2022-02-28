959cc8a650b727457164849c67aacf273badc7a4
julianamewes <95439636+julianamewes@users.noreply.github.com>
2022-02-28 16:37:30 -0300

    Estados com arrays

--- project/src/App.js
+++ project/src/App.js
@@ -1,79 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-// a função App está criando um componente react (precisa começar com letra maiúscula)
-// o que está dentro de chaves é executado como código, o que está fora de chaves é executado como texto
-// todo componente react precisa ter um componente root (raiz)
-
-// estado: dados que o componente utiliza
-
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {1+1} Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    //super() é pra chamar o constructor da clase component, já que a classe app extende a classe component
-    this.state = {
-      name: 'Juliana Mewes',
-      counter: 0
+  state = {
+      posts:[
+        {
+          id: 1,
+          title: 'O título 1',
+          body: 'O corpo 1'
+        },
+        {
+          id: 2,
+          title: 'O título 2',
+          body: 'O corpo 2'
+        },
+        {
+          id: 3,
+          title: 'O título 3',
+          body: 'O corpo 3'
+        },
+
+      ]
     };
-  }
 
-  handlePClick = () => { //arrow fuction, não precisa delcarar o this fora.
-    this.setState({name: 'Morais'});
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault(); //impede que seja feito o que seria realizado em handleAClick, e garante que será feito oq ue se pede abaixo
-    const {counter} = this.state;
-    this.setState({counter: counter + 1});
-
-  }
-
-  //quando o estado mudar, a função render será chamada
   render() {
-    const {name, counter} = this.state;
+    const {posts} = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>     
+        ))} 
       </div>
-    )
+    );
   }
 }
+
+//o map deverá ser acompanhado pelo key
 
 export default App;
