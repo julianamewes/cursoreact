e66550175896d3eb8577256081f326d5cb1d9633
Juliana Morais Mewes <juliana.teofilos@gmail.com>
2022-03-02 11:42:18 -0300

    Pros e organização dos ocmponentes

--- project/estados.js
+++ project/estados.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Component } from 'react';
 
+// para estudar: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+
 class ContadorHora extends Component {
     constructor(props) {
       super(props);
